9d9f1c7f6618f911b8922b84c7fce907b71a7b25
Toby Law <toby.law@anz.com>
2022-08-31 17:48:36 +1000

    Finished unmarshaller

--- csv/csv.go
+++ csv/csv.go
@@ -1,5 +1,83 @@
 package csv
 
-func Unmarshal(data []string, s any) error {
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+)
+
+// Unmarshal maps the data in a slice of slice of strings into a slice of structs.
+// The first row is assumed to be the header with the column names
+func Unmarshal(data [][]string, s any) error {
+	sliceValPtr := reflect.ValueOf(s)
+	if sliceValPtr.Kind() != reflect.Pointer {
+		return errors.New("must be a pointer to a slice of structs")
+	}
+	sliceVal := sliceValPtr.Elem()
+	if sliceVal.Kind() != reflect.Slice {
+		return errors.New("must be a pointer to a slice of structs")
+	}
+	structType := sliceVal.Type().Elem()
+	if structType.Kind() != reflect.Struct {
+		return errors.New("must be a pointer to a slice of structs")
+	}
+
+	// Assume the first row is a header
+	header := data[0]
+	dataCol := make(map[string]int, len(header))
+	for i, name := range header {
+		dataCol[name] = i
+	}
+
+	for _, record := range data[1:] {
+		newVal := reflect.New(structType).Elem()
+		err := unmarshalOne(record, dataCol, newVal)
+		if err != nil {
+			return err
+		}
+		sliceVal.Set(reflect.Append(sliceVal, newVal))
+	}
+
+	return nil
+}
+
+func unmarshalOne(record []string, dataCol map[string]int, vv reflect.Value) error {
+	vt := vv.Type()
+	for i := 0; i < vv.NumField(); i++ {
+		typeField := vt.Field(i)
+		col, ok := dataCol[typeField.Tag.Get("csv")]
+		if !ok {
+			continue
+		}
+		val := record[col]
+
+		field := vv.Field(i)
+		switch field.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			i, err := strconv.ParseInt(val, 10, 64)
+			if err != nil {
+				return err
+			}
+			field.SetInt(i)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			i, err := strconv.ParseUint(val, 10, 64)
+			if err != nil {
+				return err
+			}
+			field.SetUint(i)
+		case reflect.String:
+			field.SetString(val)
+		case reflect.Bool:
+			b, err := strconv.ParseBool(val)
+			if err != nil {
+				return err
+			}
+			field.SetBool(b)
+		default:
+			return fmt.Errorf("cannot handle field of kind %v", field.Kind())
+		}
+	}
+
 	return nil
 }
--- csv/csv_test.go
+++ csv/csv_test.go
@@ -3,10 +3,13 @@ package csv
 import (
 	"encoding/csv"
 	"os"
+	"reflect"
+	"strings"
 	"testing"
 )
 
-func TestUnmarshal(t *testing.T) {
+// TODO: remove
+func TestRead(t *testing.T) {
 	f, err := os.Open("testdata/data.csv")
 	if err != nil {
 		t.Fatal("Unable to read data file", err)
@@ -25,4 +28,55 @@ func TestUnmarshal(t *testing.T) {
 	}
 
 	t.Log(records)
+}
+
+func TestUsage(t *testing.T) {
+	// TODO: Example of using this package
+}
+
+// TODO: how would you refactor this?
+func TestUnmarshal(t *testing.T) {
+	data := `
+name,age,dob,city,has_pet
+Harry,"50",23/08/1965,Melbourne,true
+"Robert ""Bob"" Thompson",69,31/12/1999,Sydney,false
+Tom,2,28/02/1993,Brisbane,true
+`
+	type Schema struct {
+		Name   string `csv:"name"`
+		Age    uint8  `csv:"age"`
+		Dob    string `csv:"dob"`
+		City   string `csv:"city"`
+		HasPet bool   `csv:"has_pet"`
+	}
+
+	expected := []Schema{
+		{"Harry", 50, "23/08/1965", "Melbourne", true},
+		{"Robert \"Bob\" Thompson", 69, "31/12/1999", "Sydney", false},
+		{"Tom", 2, "28/02/1993", "Brisbane", true},
+	}
+
+	r := csv.NewReader(strings.NewReader(data))
+	records, err := r.ReadAll()
+	if err != nil {
+		t.Fatal("Failed to read records", err)
+	}
+	if len(records) != 4 {
+		t.Fatal("Read wrong number of records", len(records))
+	}
+
+	var s []Schema
+	err = Unmarshal(records, &s)
+	if err != nil {
+		t.Fatal("Failed to unmarshal records", records, err)
+	}
+	if len(s) != 4 {
+		t.Fatal("Unmarshalled the wrong number of records", len(s))
+	}
+
+	for i, record := range s {
+		if !reflect.DeepEqual(record, expected[i]) {
+			t.Fatal("Unmarshalled struct didn't match expectations", s, expected[i])
+		}
+	}
 }
